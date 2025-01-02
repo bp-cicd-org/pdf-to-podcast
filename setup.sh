30657fc7540db207e8d3a559eafae73156d44ec9
tmonty12 <tjmontfort12@gmail.com>
2025-01-02 13:49:15 -0800

    fixes to launchable

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ if ! command -v uv &> /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
 
+# Add uv to path
+source $HOME/.local/bin/env
+
 # Create a new virtual environment
 uv venv
 
